ab3536477ff9104c0e9d92321f8953725599afd3
nicoki2004 <nicoki2004@gmail.com>
2026-03-05 10:17:27 -0300

    Refactor types location

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,4 +1,5 @@
 import { Cache } from "./pokecache.js";
+import { ShallowLocations, Location } from "./ShallowLocations.js";
 
 export class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
@@ -62,66 +63,3 @@ export class PokeAPI {
     }
   }
 }
-
-export type ShallowLocations = {
-  count: number;
-  next: string;
-  previous: string;
-  results: {
-    name: string;
-    url: string;
-  }[];
-};
-
-export type Location = {
-  encounter_method_rates: {
-    encounter_method: {
-      name: string;
-      url: string;
-    };
-    version_details: {
-      rate: number;
-      version: {
-        name: string;
-        url: string;
-      };
-    }[];
-  }[];
-  game_index: number;
-  id: number;
-  location: {
-    name: string;
-    url: string;
-  };
-  name: string;
-  names: {
-    language: {
-      name: string;
-      url: string;
-    };
-    name: string;
-  }[];
-  pokemon_encounters: {
-    pokemon: {
-      name: string;
-      url: string;
-    };
-    version_details: {
-      encounter_details: {
-        chance: number;
-        condition_values: any[];
-        max_level: number;
-        method: {
-          name: string;
-          url: string;
-        };
-        min_level: number;
-      }[];
-      max_chance: number;
-      version: {
-        name: string;
-        url: string;
-      };
-    }[];
-  }[];
-};
